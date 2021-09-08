cdf531ebd2d890f7504afeb8156bca0d3af1aa74
Damian <pokemonmew34567@gmail.com>
2021-09-08 01:53:08 -0700

    Manually created Booklist. Working on book card displays

---
+++ main.js
@@ -0,0 +1,23 @@
+
+
+let myLibrary = [
+    new Book("Crying in H Mart", "Michelle Zauner", 256, false),
+    new Book("Things We Lost to the Water", "Eric Nguyen", 304, true),
+    new Book("The Four Winds", "Kristin Hannah", 320, false)
+];
+
+function Book(title, author, pgs, read) {
+    this.title = title;
+    this.author = author;
+    this.pgs = pgs;
+    this.read = read;
+}
+
+
+function addBook() {
+
+}
+
+function removeBook() {
+
+}
